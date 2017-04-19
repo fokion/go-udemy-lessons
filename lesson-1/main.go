814ecd47a2eb2ccccfb917bdab690f8473422ef4
Fokion Sotiropoulos <fokion.s@gmail.com>
2017-04-19 11:11:13 +0100

    Slice and Map

--- lesson-1/main.go
+++ lesson-1/main.go
@@ -6,4 +6,16 @@ func main() {
 	x := 7
 	fmt.Printf("%T\n", x)
 	fmt.Println(x)
+	//this is a slice of ints..
+	xi := []int{1,2,3,4,5,6}
+	fmt.Println(xi)
+	//this is a map of string with int
+	//notice the trailing comma in the
+	//last element.
+	m:=map[string]int{
+		"Fokion":28,
+		"Alexandra":26,
+		"Natalia":25,
+	}
+	fmt.Println(m)
 }
