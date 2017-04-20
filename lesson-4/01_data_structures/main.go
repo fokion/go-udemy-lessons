289b3b2b8f5d26cea2b94fcb19faccb62220385b
Fokion Sotiropoulos <fokion.s@gmail.com>
2017-04-20 17:12:40 +0100

    Range with a map

--- lesson-4/01_data_structures/main.go
+++ lesson-4/01_data_structures/main.go
@@ -12,8 +12,22 @@ func init(){
 }
 func main()  {
 	names := []string{"Fokion","Alexandra","Natalia"}
+	namesInCities := map[string]string{
+		"Edi":"Fokion",
+		"Gla":"Natalia",
+	}
 	err := tmpl.ExecuteTemplate(os.Stdout,"index.html",names)
 	if(err !=nil){
 		log.Fatal(err)
 	}
+	fl,err := os.Create("out.html")
+	if(err !=nil){
+		log.Fatal(err)
+	}
+	defer fl.Close()
+	err = tmpl.ExecuteTemplate(fl,"index.html",namesInCities)
+	if(err !=nil){
+		log.Fatal(err)
+	}
+
 }
