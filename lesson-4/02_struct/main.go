6f124ebd98223addc478b1b80f5fe213d845f7da
Fokion Sotiropoulos <fokion.s@gmail.com>
2017-04-21 10:35:36 +0100

    Slice of structs in a template

--- lesson-4/02_struct/main.go
+++ lesson-4/02_struct/main.go
@@ -15,6 +15,10 @@ type Person struct {
 	Surname string
 }
 func main()  {
+	var people = []Person{}
 	p1 := Person{"Fokion","Sotiropoulos"}
+	people = append(people,p1)
+	people = append(people,Person{"Natalia", "Sotiropoulou"})
 	tmpl.ExecuteTemplate(os.Stdout,"index.html",p1)
+	tmpl.ExecuteTemplate(os.Stdout,"index-people.html",people)
 }
